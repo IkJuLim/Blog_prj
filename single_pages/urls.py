86aa92840af9d031ebd599ded0d535d25ccba824
임익주 <ikedo@im-igjuui-MacBookPro.local>
2021-02-25 07:50:25 +0900

    creat landing and about_me pages

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from . import views
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('single_pages.urls')),
+    path('', views.landing),
+    path('about_me/', views.about_me),
 ]
