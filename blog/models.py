887c8827c5d9d39ca806c42bcfd4f130d32f575f
임익주 <ikedo@im-igjuui-MacBookPro.local>
2021-02-25 21:56:07 +0900

    Update post detail

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length = 30)
+    title = models.CharField(max_length=30)
+    sub_title = models.CharField(max_length=300, blank=True)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
