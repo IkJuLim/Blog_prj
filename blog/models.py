c79e26f818f9f6a1a17a620af55041a6e57b98ae
임익주 <ikedo@im-igjuui-MacBookPro.local>
2021-03-12 14:10:25 +0900

    add author field on post_list and post_detail page

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
